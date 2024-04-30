cd696390dd584847ae8e1028e05404e53e88c9d4
Jacob Jennings <jacob.r.jennings@gmail.com>
2024-04-30 11:38:50 -0500

    Training loop exists, results are poor! Progress!

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -1,57 +1,121 @@
 import argparse
+import copy
 import os
 import logging
 import secrets
 import numpy
+import copy
+import threading
+
+from ipywidgets import IntProgress
+from IPython.display import display
+from IPython.display import Audio
+import time
+
+from more_itertools import chunked
+from multiprocessing import Pool, TimeoutError, Value
+
+# PyTorch model and training necessities
 import torch
+import torch.nn as nn
+import torch.nn.functional as nnF
+import torch.optim as optim
+from torch.utils.data import DataLoader
 
-from datasets import load_dataset, Audio
+# Audio
+import torchaudio
+from torio.io import CodecConfig
+
+# Image datasets and image manipulation
+import torchvision
+import torchvision.transforms as transforms
+
+# Image display
+import matplotlib.pyplot as plt
+import numpy as np
+
+# PyTorch TensorBoard support
+from torch.utils.tensorboard import SummaryWriter
+
+print(torch.__version__)
+print(torchaudio.__version__)
+
+base_dataset_directory = '/home/jacob/cv-corpus-17.0-2024-03-15/en'
+# noisy_dataset_directory = '/home/jacob/noisy-commonvoice/en'
+
+### Load base dataset
+
+training_speech_dataset = torchaudio.datasets.COMMONVOICE(root=base_dataset_directory)
+
+# torch.set_default_dtype()
+torch.manual_seed(314)
 
 
-def add_noise(batch):
-    audio = batch['audio']
-    noise = numpy.asarray(0.01 * numpy.random.randn(len(audio['array'])))
-    audio['array'] = audio['array'] + noise
-    return batch
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
 
-# Parse and define arguments
-parser = argparse.ArgumentParser(
-                    prog='prepare_data',
-                    description='Prepares datasets for clarification training.')
+add_noise = AddGaussianNoise(std=0.5)
 
-# Shorthand forms omitted intentionally for clarity.
-parser.add_argument('--base_dataset_cache_directory', required=True)
-parser.add_argument('--modified_dataset_cache_directory', required=True)
-args = parser.parse_args()
+training_speech_dataset_noisy = copy.deepcopy(training_speech_dataset)
 
-# Set up log file which should appear in the working directory.
-logging.basicConfig(filename="prepare_data.log", encoding="utf-8", level=logging.INFO)
+data_loader = DataLoader(
+    training_speech_dataset_noisy,
+    batch_size=1)
 
-# Additional argument validations
-if not args.base_dataset_cache_directory.endswith("/"):
-    logging.error("base_dataset_cache_directory must end with a / character.")
+print(torchaudio.utils.ffmpeg_utils.get_audio_encoders())
 
-if not args.modified_dataset_cache_directory.endswith("/"):
-    logging.error("modified_dataset_cache_directory must end with a / character.")
+print(torchaudio.list_audio_backends)
 
-# Load base dataset from cache, or download it (downloads everything)
-training_speech_dataset = load_dataset(
-    path="fsicoli/common_voice_17_0",
-    name="en",
-    split="train[:1000]",
-    cache_dir=os.path.dirname(args.base_dataset_cache_directory),
-    trust_remote_code=True
-)
+print(f"Detected {os.cpu_count()} cpus.")
 
-training_speech_audio_dataset = training_speech_dataset.cast_column('audio', Audio(sampling_rate=16000))
-training_speech_audio_dataset.map(add_noise)
+data_loader_len = len(data_loader)
 
-training_speech_dataset.save_to_disk(os.path.dirname(args.modified_dataset_cache_directory))
+num_processed = Value("l", 0)
 
-logging.info("training_speech_dataset: {}".format(training_speech_dataset))
-logging.info("first element: {}".format(training_speech_dataset[0]["audio"]))
+t0 = time.time()
 
-dataset = training_speech_dataset.cast_column("audio", Audio(sampling_rate=16000))
-logging.info("first element: {}".format(dataset[0]["audio"]))
+process_count = os.cpu_count()
 
+def noisify_file(data):
+    if num_processed.value % 1000 == 0 and num_processed.value != 0:
+        t1 = time.time()
+        elapsed = t1 - t0
+        files_per_second = num_processed.value / elapsed
+        print(
+            f"Processed {num_processed.value}/{data_loader_len} files "
+            f"({num_processed.value / data_loader_len * 100:.0f}%). "
+            f"Elapsed: {elapsed:.0f}s. "
+            f"Files/second: {files_per_second:.0f}. "
+            f"Remaining estimated hours: {(data_loader_len - num_processed.value) / files_per_second / 60 / 60:.2f}")
+
+    num_processed.value += 1
+    filename = data[2]["path"][0]
+    noisy_waveform = add_noise(data[0].squeeze())
+
+    noisy_waveform = noisy_waveform.unsqueeze(1)
+
+    src = noisy_waveform
+
+    torchaudio.save(
+        uri=f"{noisy_dataset_directory}/clips/{filename}",
+        src=src,
+        sample_rate=data[1][0].item(),
+        format="mp3",
+        channels_first=False
+    )
+
+    return 0
+
+
+with Pool(processes=process_count) as pool:
+    for i in pool.imap_unordered(noisify_file, data_loader, chunksize=os.cpu_count()):
+        pass
